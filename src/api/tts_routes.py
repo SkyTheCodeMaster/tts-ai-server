73901c8aece6fa2a0326a9d5aec486162ee54a7e
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2024-02-03 00:01:27 -0500

    Add more logging

--- src/api/tts_routes.py
+++ src/api/tts_routes.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
+import time
 from typing import TYPE_CHECKING
 
 from aiohttp import web
 from aiohttp.web import Response
 
-from utils.generate_tts import models, generate, DEFAULT_MODEL
+from utils.generate_tts import DEFAULT_MODEL, generate, models
 
 if TYPE_CHECKING:
   from utils.extra_request import Request
@@ -24,7 +25,11 @@ async def post_ai_generate(request: Request) -> Response:
   if voice not in models:
     return Response(status=400,text="Passed voice not in list of models! Use GET /ai/models/ endpoint for voice list.")
 
+  request.LOG.info("Starting generation job for {0} with input text: '{1}'", request.remote, text)
+  start_time = time.time()
   wav_bytes = await generate(text, voice)
+  end_time = time.time()
+  request.LOG.info("Finished generation job for {0} with input text: '{1}'.\nGeneration took {2}s.", request.remote, text, end_time-start_time)
 
   return Response(body=wav_bytes, status=200, content_type="audio/x-wav")
 
